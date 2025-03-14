246dcee841e083b32a31756fd54749d9a5de94ef
liutao <email.lt@qq.com>
2025-03-14 11:25:43 +0800

    feat:缺省路由不生成文档

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.include_router(book_controller.router)
 
 ExceptionHandler.register_exception_handler(app)
 
-@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"])
+@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"], include_in_schema=False)
 def default_route(path: str):
     """缺省路由"""
     return Response.error(message=f"{path} not found", code=StatusEnum.error)
