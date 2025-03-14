5ffcaa7197fcf0edace660b21a06fa0f190c79a5
liutao <email.lt@qq.com>
2025-03-14 11:42:01 +0800

    feat:单元测试

---
+++ tests/app/controller/test_book_controller.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from fastapi.testclient import TestClient
+
+from core.status_enum import StatusEnum
+from main import app
+
+
+class BookControllerTest(TestCase):
+    client = TestClient(app)
+
+    def test_get(self):
+        response = self.client.post("/api/book/get", json={"id": 1})
+        assert response.status_code == 200
+        assert response.json()["code"] == StatusEnum.success.value
+        assert response.json()["data"] is not None
+
+
+    def test_list_book(self):
+        response = self.client.post("/api/book/list", json={"page": 1, "limit": 1})
+        assert response.status_code == 200
+        assert response.json()["code"] == StatusEnum.success.value
+        assert isinstance(response.json()["data"]["data"], list)
+        assert response.json()["data"]["total"] >= len(response.json()["data"]["data"])
