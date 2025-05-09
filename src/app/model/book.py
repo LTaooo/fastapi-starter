f50f7804977f903bfcca36c89323c4c67c1c5ce9
liutao <tliu@suntekcorps.com>
2025-05-09 09:04:12 +0800

    feat: use src layout

--- src/app/model/book.py
+++ src/app/model/book.py
@@ -1,7 +1,13 @@
+from typing import Type
+
 from sqlmodel import SQLModel, Field
 
 
 class Book(SQLModel, table=True):
+    @classmethod
+    def __tablename__(cls: Type['Book']) -> str:
+        return 'book'
+
     id: int = Field(default=None, primary_key=True)
     name: str = Field(max_length=255, nullable=False, description='书名')
     created_at: int = Field(nullable=False, description='创建时间')
