4ad26446267306294dc08fb248f6689fcc334d57
liutao <tliu@suntekcorps.com>
2025-05-12 16:20:17 +0800

    feat: support nacos

--- src/config/nacos_config.py
+++ src/config/nacos_config.py
@@ -5,13 +5,13 @@ from config.app_config import AppConfig
 from core.config import Config
 
 
+_app_config = Config.get(AppConfig)
+
+
 class NacosService(BaseModel):
     name: str = Field(description='服务名称')
-    ip: str = Field(description='服务IP')
     port: int = Field(description='服务端口')
-
-
-_app_config = Config.get(AppConfig)
+    ip: str = Field(description='服务IP', default='127.0.0.1')
 
 
 class NacosConfig(BaseSettings):
@@ -20,4 +20,4 @@ class NacosConfig(BaseSettings):
     server_addresses: str = Field(description='Nacos服务器地址', alias='NACOS_SERVER_ADDRESSES', default='127.0.0.1:8848')
     namespace: str = Field(description='Nacos命名空间', alias='NACOS_NAMESPACE', default='dev')
     group: str = Field(description='Nacos组', alias='NACOS_GROUP', default='DEFAULT_GROUP')
-    services: list[NacosService] = [NacosService(name=_app_config.app_name, ip=_app_config.host, port=_app_config.port)]
+    services: list[NacosService] = [NacosService(name=_app_config.app_name, port=_app_config.port)]
