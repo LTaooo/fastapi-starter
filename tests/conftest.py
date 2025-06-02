f3d706434197940a0b59837bd024c301a330ec64
liutao <tliu@suntekcorps.com>
2025-06-02 16:40:00 +0800

    fix: update

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,5 +11,6 @@ from core.mysql.database.book.book_database import BookDatabase
 async def client() -> AsyncGenerator[AsyncClient, None]:
     """提供一个在整个测试会话中共享的 HTTP 客户端"""
     async with AsyncClient(transport=ASGITransport(app=app), base_url='http://127.0.0.1:8000') as ac:
+        BookDatabase.init()
         yield ac
-        await BookDatabase().close()
+        await BookDatabase.close()
