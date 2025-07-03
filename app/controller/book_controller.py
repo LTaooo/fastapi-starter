57d3d74cb152a311630ed68baa152f0872ddc722
liutao <tliu@suntekcorps.com>
2025-07-03 11:13:51 +0800

    123

--- app/controller/book_controller.py
+++ app/controller/book_controller.py
@@ -1,23 +1,16 @@
 from fastapi import APIRouter
 
-from app.dto.request.book_create_req import BookCreateReq
 from core.dto.common_res import CommonRes
-from core.dto.page_res import PageRes
 from app.dto.request.book_get_req import BookGetReq
-from app.dto.request.book_list_req import BookListReq
 from app.dto.response.book_get_res import BookGetRes
-from app.service.book_service import BookService
 from core.mcp.operation_enums import OperationEnum
-from core.mysql.database.book.book_database import BookDatabase
-from core.mysql.database.book.book_session import BookSession
 from core.response import Response
-from fastapi import Depends
 
 router = APIRouter(prefix='/api/book', tags=['book'])
 
 
 class BookController:
     @staticmethod
-    @router.post('/get', summary='根据id获取书', operation_id=OperationEnum.get_book.value.id)
+    @router.post('/get', summary='根据id获取书, 返回书籍详情', operation_id=OperationEnum.get_book.value.id)
     async def get(param: BookGetReq) -> CommonRes[BookGetRes]:
         return Response.success('hello')
